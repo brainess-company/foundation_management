85e2f6ff3343802c7d950bfa19b27d8b8cc29a49
BrainessCompany <brainesscompany@gmail.com>
2024-04-17 01:01:57 -0300

    atualizei o arquivo foundation_estacas, com a lógica para selecionar apenas o diametro que está relacionado com o serviço dentro da sale order mas ainda não testei

--- models/foundation_estacas.py
+++ models/foundation_estacas.py
@@ -1,12 +1,27 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class FoundationEstacas(models.Model):
     _name = 'foundation.estacas'
     _description = 'Estacas utilizadas na obra'
 
-    foundation_obra_service_id = fields.Many2one('foundation.obra.service', string="Serviço na Obra", required=True)
+    foundation_obra_service_id = fields.Many2one(
+        'foundation.obra.service', string="Serviço na Obra", required=True)
     nome_estaca = fields.Char("Nome da Estaca", required=True)
-    diametro = fields.Float("Diâmetro (cm)")
+    diametro = fields.Selection(
+        selection='_compute_diametros', string="Diâmetro (cm)",
+        help="Diâmetros disponíveis baseados nas variantes do produto na Ordem de Venda relacionada.")
     profundidade = fields.Float("Profundidade (m)", required=True)
     data = fields.Date("Data")
     observacao = fields.Char("Observação")
+
+    @api.depends('foundation_obra_service_id')
+    def _compute_diametros(self):
+        for record in self:
+            res = []
+            if record.foundation_obra_service_id and record.foundation_obra_service_id.sale_order_id:
+                sale_order = record.foundation_obra_service_id.sale_order_id
+                product_ids = sale_order.order_line.mapped('product_id').filtered(lambda p: p.product_tmpl_id == record.foundation_obra_service_id.service_id.product_tmpl_id)
+                res = [(str(product.id), product.display_name) for product in product_ids]
+            record.diametro = [(0, '')] + res  # add a default empty value
+
+
