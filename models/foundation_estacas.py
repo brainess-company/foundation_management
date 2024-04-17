4e074f55d5f00b875f50c776860e9b64d3023538
BrainessCompany <brainesscompany@gmail.com>
2024-04-17 10:59:15 -0300

    a view de form está incluindo o notebook para cada foundation.obra.service e permite incluir uma estaca relaciona

--- models/foundation_estacas.py
+++ models/foundation_estacas.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api
+from odoo import models, fields
+
 
 class FoundationEstacas(models.Model):
     _name = 'foundation.estacas'
@@ -7,21 +8,11 @@ class FoundationEstacas(models.Model):
     foundation_obra_service_id = fields.Many2one(
         'foundation.obra.service', string="Serviço na Obra", required=True)
     nome_estaca = fields.Char("Nome da Estaca", required=True)
-    diametro = fields.Selection(
-        selection='_compute_diametros', string="Diâmetro (cm)",
-        help="Diâmetros disponíveis baseados nas variantes do produto na Ordem de Venda relacionada.")
+    diametro = fields.Float("Diâmetro (cm)")
     profundidade = fields.Float("Profundidade (m)", required=True)
     data = fields.Date("Data")
     observacao = fields.Char("Observação")
 
-    @api.depends('foundation_obra_service_id')
-    def _compute_diametros(self):
-        for record in self:
-            res = []
-            if record.foundation_obra_service_id and record.foundation_obra_service_id.sale_order_id:
-                sale_order = record.foundation_obra_service_id.sale_order_id
-                product_ids = sale_order.order_line.mapped('product_id').filtered(lambda p: p.product_tmpl_id == record.foundation_obra_service_id.service_id.product_tmpl_id)
-                res = [(str(product.id), product.display_name) for product in product_ids]
-            record.diametro = [(0, '')] + res  # add a default empty value
+
 
 
