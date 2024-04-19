c54dd3c2d57f5deed390b3cfd2a8d00de715b6fd
BrainessCompany <brainesscompany@gmail.com>
2024-04-19 02:51:56 -0300

    inseri chatter na tela de maquinas e rastreeu campos

--- models/foundation_maquina.py
+++ models/foundation_maquina.py
@@ -6,6 +6,6 @@ class FoundationMaquina(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']  # Herdar de mail.thread e mail.activity.mixin
     _rec_name = 'nome_maquina'
 
-    nome_maquina = fields.Char("Máquina")
-    operador = fields.Many2one('res.partner', string="Operador")
-    observacao = fields.Char("Observação")
+    nome_maquina = fields.Char("Máquina", track_visibility='onchange')
+    operador = fields.Many2one('res.partner', string="Operador", track_visibility='onchange')
+    observacao = fields.Char("Observação", track_visibility='onchange')
