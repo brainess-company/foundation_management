d95a5d065ddd6b17690656f508b523046dfe77ce
BrainessCompany <brainesscompany@gmail.com>
2024-04-19 02:45:54 -0300

    inseri chatter na tela de maquinas

--- models/foundation_maquina.py
+++ models/foundation_maquina.py
@@ -3,6 +3,7 @@ from odoo import models, fields
 class FoundationMaquina(models.Model):
     _name = 'foundation.maquina'
     _description = 'Cadastro de Máquinas'
+    _inherit = ['mail.thread', 'mail.activity.mixin']  # Herdar de mail.thread e mail.activity.mixin
     _rec_name = 'nome_maquina'
 
     nome_maquina = fields.Char("Máquina")
