6fddd28a1736170a6d8e39e955f6e87d43fdd39c
BrainessCompany <brainesscompany@gmail.com>
2024-04-24 02:13:54 -0300

    inseri um menu em sale order

--- __manifest__.py
+++ __manifest__.py
@@ -27,6 +27,9 @@
         'views/foundation_actions.xml',  # Load actions (this should come early, before other views)
         'views/foundation_menus.xml',  # Load menu definitions
         'views/sale_order_obra_extension.xml',
+        #'views/sale_order_obra_extension_link_order.xml',
+        #'views/sale_order_obra_extension_link_invoice.xml',
+        #'views/sale_order_obra_extension.xml',
     ],
 
     'demo': [],
