1babb25cf0225c7ea584e40b2cd4981fc0d035ca
BrainessCompany <brainesscompany@gmail.com>
2024-04-25 02:12:37 -0300

    agora esta computando o numero de funcionarios na equipe

--- models/foundation_maquina.py
+++ models/foundation_maquina.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class FoundationMaquina(models.Model):
     _name = 'foundation.maquina'
@@ -9,3 +9,16 @@ class FoundationMaquina(models.Model):
     nome_maquina = fields.Char("Máquina",  tracking=True)
     operador = fields.Many2one('res.partner', string="Operador",  tracking=True)
     observacao = fields.Char("Observação")
+    team_ids = fields.One2many('foundation.team', 'machine_id', string="Histórico de Equipes")
+    current_team_employees = fields.Many2many('res.partner', string="Equipe Atual",
+                                              compute='_compute_current_team_employees',
+                                              store=False)
+
+
+    @api.depends('team_ids')
+    def _compute_current_team_employees(self):
+        for machine in self:
+            last_team = self.env['foundation.team'].search([
+                ('machine_id', '=', machine.id)],
+                order='date desc', limit=1)
+            machine.current_team_employees = last_team.employee_ids if last_team else False
