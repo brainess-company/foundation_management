55bf598b102a5345236c148d56220154cbe694fd
BrainessCompany <brainesscompany@gmail.com>
2024-04-29 12:53:00 -0300

    corrigi a aba de teste para ajuda, um link para download da descrição e um link para o site, falta incluir um link para suporte

--- __manifest__.py
+++ __manifest__.py
@@ -12,14 +12,15 @@
         'base',
         'sale',
         'mail',
+        'website',
         'product',
         'account',  # Para usar contas analíticas
         'hr',       # Se você estiver usando funcionalidades relacionadas a empregados
     ],
     'data': [
-
-        'security/ir.model.access.csv',  # Load security and access rules first
         'views/help_page_template.xml',
+        'security/ir.model.access.csv',  # Load security and access rules first
+
         'views/freports.xml',  # Load other reports or final views
         'views/foundation_maquina_view.xml',  # Load views for the Maquina model
         'views/foundation_obra_view.xml',  # Load views for the Obra model
