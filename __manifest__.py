8ad70d03c32ec9edce6307434db5a416338ba5b9
BrainessCompany <brainesscompany@gmail.com>
2024-05-07 09:21:00 -0300

    ocultei em a barra de aprovação de relatório, ela só aparece para engenharia

--- __manifest__.py
+++ __manifest__.py
@@ -21,6 +21,7 @@
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
+        # 'security/sale_order_rules.xml',
         'views/foundation_stock_move_wizard_view.xml',
         'views/foundation_chamada_view.xml',
         'views/foundation_saldo_estoque_view.xml',
