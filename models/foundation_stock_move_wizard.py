f7b08f69786d2e80b86a1351ef31c05ed65e0de2
BrainessCompany <brainesscompany@gmail.com>
2024-05-09 12:38:22 -0300

    estoque funcionando perfeitamente

--- models/foundation_stock_move_wizard.py
+++ models/foundation_stock_move_wizard.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class StockMoveWizard(models.TransientModel):
@@ -12,7 +13,8 @@ class StockMoveWizard(models.TransientModel):
         domain="[('id', 'in', available_product_ids)]"
     )
     product_qty = fields.Float(string="Quantidade", default=1.0)
-    maquina_registro_id = fields.Many2one('foundation.maquina.registro', string="Dados de Origem", required=True)
+    maquina_registro_id = fields.Many2one('foundation.maquina.registro', string="Dados de Origem",
+                                          required=True)
     available_product_ids = fields.Many2many(
         'product.product',
         compute='_compute_available_product_ids',
@@ -24,13 +26,26 @@ class StockMoveWizard(models.TransientModel):
         for wizard in self:
             if wizard.maquina_registro_id:
                 stock_location_id = wizard.maquina_registro_id.specific_stock_location_id.id
-                quants = wizard.env['stock.quant'].search([('location_id', '=', stock_location_id)])
+                quants = wizard.env['stock.quant'].search(
+                    [('location_id', '=', stock_location_id), ('quantity', '>', 0)])
                 wizard.available_product_ids = quants.mapped('product_id')
             else:
                 wizard.available_product_ids = self.env['product.product']
 
     def action_create_stock_move(self):
         self.ensure_one()
+
+        # Obtém a quantidade disponível no local de origem
+        stock_location_id = self.maquina_registro_id.specific_stock_location_id.id
+        quant = self.env['stock.quant'].search(
+            [('location_id', '=', stock_location_id), ('product_id', '=', self.product_id.id)],
+            limit=1)
+        available_qty = quant.quantity if quant else 0.0
+
+        if self.product_qty > available_qty:
+            raise UserError(
+                f"Quantidade solicitada ({self.product_qty}) excede a quantidade disponível ({available_qty}) para o produto '{self.product_id.display_name}'.")
+
         stock_move = self.env['stock.move'].create({
             'name': 'Movimento via Árvore: {}'.format(self.product_id.display_name),
             'product_id': self.product_id.id,
@@ -44,8 +59,9 @@ class StockMoveWizard(models.TransientModel):
         stock_move._action_assign()
         stock_move.quantity_done = self.product_qty
         stock_move._action_done()
+
         return {
-            'type': 'ir.actions.act_window',
+            'type': 'ir.actions.act_window_close',
             'name': 'Movimento de Estoque',
             'res_model': 'stock.move',
             'view_mode': 'form',
