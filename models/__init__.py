9385d18ac3aceca640a0781e4181000f5d279386
BrainessCompany <brainesscompany@gmail.com>
2024-06-12 10:12:50 -0300

    comentei a linha do init que carrega o stock pickin extension

--- models/__init__.py
+++ models/__init__.py
@@ -23,7 +23,7 @@ from . import foundation_stock_quant_extend
 from . import foundation_employee_extend
 from . import foundation_employee_assignment
 from . import foundation_hrdepartment_extend
-from . import foundation_extend_stock_picking
+# from . import foundation_extend_stock_picking
 from . import foundation_maquina_obra
 from . import product_template
 # from . import foundation_extension_sale_order_obra
