4ff74829c3fcc19f0bc30a556088a2959bc1bd62
BrainessCompany <brainesscompany@gmail.com>
2024-06-12 11:56:29 -0300

    atualizei a string da sale order extencion

--- models/foundation_obra_extension.py
+++ models/foundation_obra_extension.py
@@ -5,6 +5,16 @@ _logger = logging.getLogger(__name__)
 
 
 class SaleOrder(models.Model):
+    """
+    Herdaa o sale order e quando confirmado:
+    cria estoque central
+    cria estoque para a obra
+    cria um registro na tabela foundation obra
+    cria varios registros em foundation obra service, um pra cada serviço
+    cria ou atualiza plano de conta e contas analiticas e conta analitica para cada serviço distinto
+        e vincula cada conta analitoca com a sale order, isso para todas as empresas no sistema
+        o usurio q confirma a sale order tem q ter permissão em todas as empresas
+    """
     _inherit = 'sale.order'
     nome_obra = fields.Char("Nome da Obra", required=True)
     specific_stock_location_id = fields.Many2one('stock.location',
