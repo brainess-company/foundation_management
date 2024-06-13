1fbc307af7daf44c5605739a3f6b7d52ea6fb613
BrainessCompany <brainesscompany@gmail.com>
2024-06-13 10:53:48 -0300

    removi arquivos desnecessários

--- models/foundation_maintenance_equipment_extend.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class MaintenanceEquipment(models.Model):
-    _inherit = 'maintenance.equipment'
-
-    maquina_id = fields.Many2one('foundation.maquina', string="Máquina Associada")
--- models/product_template.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    criar_conta_analitica = fields.Boolean(string='Criar Conta Analítica')
