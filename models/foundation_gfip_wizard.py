21d564ac09104efe389327acbae950f9b6e89d40
BrainessCompany <brainesscompany@gmail.com>
2024-07-11 10:16:36 -0300

    inclui mais campos na gfip

--- models/foundation_gfip_wizard.py
+++ models/foundation_gfip_wizard.py
@@ -16,7 +16,7 @@ class ReportWizard(models.TransientModel):
             ('data', '<=', self.date_end)
         ])
 
-        # Dicionário para armazenar a contagem de dias únicos de presença por funcionário, obra e empresa
+        # Dicionário para armazenar a contagem de dias únicos de presença por funcionário, obra, empresa, máquina, CNPJ e CEI
         presence_count = {}
 
         for chamada in chamadas:
@@ -24,9 +24,13 @@ class ReportWizard(models.TransientModel):
                 funcionario_id = lista_presenca.funcionario_id.id
                 obra_id = chamada.obra_id.id
                 company_id = lista_presenca.company_id.id
+                maquina_id = chamada.maquina_id.id
+                sale_order = chamada.sale_order_id
+                cnpj = sale_order.partner_id.company_registry
+                cei = sale_order.cei
                 data_presenca = chamada.data
 
-                key = (funcionario_id, obra_id, company_id)
+                key = (funcionario_id, obra_id, company_id, maquina_id, cnpj, cei)
                 if key not in presence_count:
                     presence_count[key] = set()
                 presence_count[key].add(data_presenca)
@@ -36,20 +40,24 @@ class ReportWizard(models.TransientModel):
         sheet = workbook.add_worksheet()
 
         # Adicionar cabeçalhos
-        headers = ['Funcionário', 'Obra', 'Empresa', 'Quantidade de Dias']
+        headers = ['Funcionário', 'Obra', 'Empresa', 'Máquina', 'CNPJ', 'CEI', 'Quantidade de Dias']
         for col_num, header in enumerate(headers):
             sheet.write(0, col_num, header)
 
         row = 1
         for key, days in presence_count.items():
-            funcionario_id, obra_id, company_id = key
+            funcionario_id, obra_id, company_id, maquina_id, cnpj, cei = key
             funcionario = self.env['hr.employee'].browse(funcionario_id)
             obra = self.env['foundation.obra'].browse(obra_id)
             company = self.env['res.company'].browse(company_id)
+            maquina = self.env['foundation.maquina'].browse(maquina_id)
             sheet.write(row, 0, funcionario.name)
             sheet.write(row, 1, obra.nome_obra)
             sheet.write(row, 2, company.name)
-            sheet.write(row, 3, len(days))
+            sheet.write(row, 3, maquina.nome_maquina)
+            sheet.write(row, 4, cnpj)
+            sheet.write(row, 5, cei)
+            sheet.write(row, 6, len(days))
             row += 1
 
         workbook.close()
